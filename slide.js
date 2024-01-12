e34c6a5bfbc1e2c80d8bb32dcdfcdc5bd6a23a1a
kuanwei2 <95508793+kevin0411@users.noreply.github.com>
2024-01-12 16:27:39 -0600

    basic

--- slide.js
+++ slide.js
@@ -15,3 +15,21 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
 }
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
